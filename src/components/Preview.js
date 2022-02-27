87ec624114738d8c64870d82d25f9d08b706af50
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-27 07:23:29 +0100

    add preview component

---
+++ src/components/Preview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/Preview.css";
+
+export default function Preview() {
+  return (
+    <div id="preview-container">
+      <label for="preview-box">Preview:</label>
+      <div id="preview-box"></div>
+    </div>
+  );
+}
