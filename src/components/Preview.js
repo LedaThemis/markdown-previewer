16d1b23db59142a44f79b95323c66e71f8ac948c
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-27 07:54:04 +0100

    pass prop to markdown component from marked-react

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Markdown from "marked-react";
 
 import "../styles/Preview.css";
 
-export default function Preview() {
+export default function Preview(props) {
   return (
     <div id="preview-container">
       <label for="preview-box">Preview:</label>
-      <div id="preview-box"></div>
+      <div id="preview-box">
+        <Markdown>{props.currentMarkdown}</Markdown>
+      </div>
     </div>
   );
 }
